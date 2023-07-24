c0dbef8e718a2e4384835c6b0682d8232e223c0a
Наумов Пётр <petia.naumov2013@gmail.com>
2023-07-24 18:16:05 +0300

    second commit

--- src/String_Calculator.java
+++ src/String_Calculator.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class StringCalculator {
+public class String_Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
@@ -24,17 +24,15 @@ class Main {
             str = str.replaceAll("\"", "");
             str = str.replace(" * ", "F");
             String[] a = str.split("F");
-            if (a[1].matches("[-+]?\\d+") && Integer.valueOf(a[1]) <= 10 || Integer.valueOf(a[1]) >= 1) {
+            if (a[1].matches("[-+]?\\d+")) {
                 sign = "*";
             }
         } else if (str.contains("\" / ") && str.startsWith("\"") && str.length() <= 16) {
             str = str.replaceAll("\"", "");
             str = str.replace(" / ", "F");
             String[] b = str.split("F");
-            if (b[1].matches("[-+]?\\d+") && Integer.valueOf(b[1]) <= 10 || Integer.valueOf(b[1]) >= 1) {
+            if (b[1].matches("[-+]?\\d+")) {
                 sign = "/";
-            } else {
-                throw new NumberFormatException("Throws exception");
             }
         } else {
             throw new NumberFormatException("Throws exception");
@@ -43,7 +41,7 @@ class Main {
         switch(sign) {
             case "+":
                 str = str.replace("\" + \"", "");
-                System.out.println(str);
+                result = str;
                 break;
             case "-":
                 str = str.replaceAll("\"", "");
@@ -56,30 +54,33 @@ class Main {
                 }
                 break;
             case "*":
-
                 String[] d = str.split("F");
-                String h = "";
                 int e = Integer.valueOf(d[1]);
+                if (e > 10 || e < 1) {
+                    throw new NumberFormatException("Throws exception");
+                }
                 String g = "";
                 for (int i = 0; i < e; i++) {
                     g = g.concat(d[0]);
-                    if (g.length() == 40){
-                        h = g;
-                    }
-                    if (g.length() <= 40 && i == e - 1) {
+                }
+                    if (g.length() <= 40) {
                         result = "\"" + g + "\"";
                         break;
-                    } else if (g.length() >= 40 && i == e - 1){
-                        result = "\"" + h + "..." + "\"";
+                    } else {
+                        result = "\"" + g.substring(0,40) + "..." + "\"";
                         break;
                     }
-                }
-                break;
+
 
             case "/":
                 String[] f = str.split("F");
+
                 int k = Integer.valueOf(f[1]);
-                f[0] = f[0].substring(0, k);
+                if (k > 10 || k < 1) {
+                    throw new NumberFormatException("Throws exception");
+                }
+                k = str.length() / k;
+                f[0] = f[0].substring(0, k - 1);
                 result = "\"" + f[0] + "\"";
                 break;
             default:
